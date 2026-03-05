9047e2a642c5c8b00de64a25ec9045aee0776ff5
SiddTheCoder <siddthecoder@gmail.com>
2026-03-05 07:27:57 +0545

    feat: Add new testing scripts for various functionalities and implement audio call automation

---
+++ server/test_rag.py
@@ -0,0 +1,18 @@
+from app.cache import cache_manager
+import json
+c = cache_manager
+user_id = "695e2bbaf8efc966aaf9f218"
+
+
+async def main():
+  rtx = await c.get_last_n_messages(user_id=user_id)
+  print(json.dumps(rtx, indent=2))
+
+  q = input("Query : ")
+  rtx = await c.process_query_and_get_context(user_id=user_id, current_query=q)
+  print(json.dumps(rtx, indent=2))
+
+if __name__ == "__main__":
+  import asyncio
+  while True:
+    asyncio.run(main())  
